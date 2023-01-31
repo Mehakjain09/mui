a51b59c5e3c4d01c5cb99f2ea74e787349a89fb6
Mehakjain09 <kalamehakjain@gmail.com>
2023-01-31 11:31:07 +0530

    donee

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,81 @@
+import { AccountCircle, Article, DarkMode, Groups, Home, PeopleAlt, Settings, Storefront } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
+import React from 'react'
+
+const Sidebar = ({mode,setMode}) => {
+  return (
+    <Box  flex={1} p={2} sx={{display:{xs:"none", sm:"block"} }}>
+      <Box sx={{position:"fixed"}}>
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Home/>
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Article/>
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Groups/>
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Storefront/>
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <PeopleAlt/>
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Settings/>
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <AccountCircle/>
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DarkMode/>
+              </ListItemIcon>
+              <Switch onChange={e=>setMode(mode==="light"?"dark":"light")}/>
+            </ListItemButton>
+          </ListItem>
+          
+      </List>
+    </Box>
+    </Box>
+  )
+}
+
+export default Sidebar
